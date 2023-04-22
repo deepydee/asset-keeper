4979c5f15c38ecc9cf08f65dda79ae3c084f82fb
Vitaliy Semenov <synthex@bk.ru>
2023-04-22 20:44:37 +0600

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AssetController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LiabilityController;
+use App\Http\Controllers\PromotionController;
+use App\Http\Controllers\SourceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +20,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function() {
+    Route::get('dashboard', DashboardController::class)->name('dashboard');
+
+    Route::get('assets/create/{date}', [AssetController::class, 'create'])
+        ->name('assets.create');
+    Route::resource('assets', AssetController::class);
+    Route::resource('liabilities', LiabilityController::class);
+    Route::resource('clients', ClientController::class);
+    Route::resource('promotions', PromotionController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('sources', SourceController::class);
+
+    Route::permanentRedirect('{any}', '/dashboard')
+        ->where('any', '.*');
 });
